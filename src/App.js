f6b84fccb9d0a7816a92337c4f6e0902136ba7d2
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2025-10-23 14:07:58 +0200

    Basic Nav

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      width: '100vw',
+      height: '100vh',
+      margin: 0,
+      padding: 0
+    }}>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
